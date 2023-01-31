7cbb66992f173297cc001ba4fed8d56c71c2b9af
Noam DE MASURE <demasure.noam@hotmail.fr>
2023-02-01 00:16:50 +0100

    trying to rework the tictactoe code to fit new event implementation

--- client/tictactoe/tictactoe.c
+++ client/tictactoe/tictactoe.c
@@ -8,7 +8,9 @@
 #include <string.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL.h>
+#include <pthread.h>
 #include "../../sdl-utils/SDLUtils.h"
+#include "../../events/EventManager.h"
 
 
 void printboard(int ** board);
@@ -17,111 +19,105 @@ int turn(int ** board, int player, int px, int py);
 SDL_Renderer * renderer = NULL;
 SDL_Window * window = NULL;
 
-SDL_bool program_launch = SDL_TRUE;
-
-typedef struct{
-    int beginX;
-    int beginY;
-    int endX;
-    int endY;
-    int actionType;
-} Button ;
-
-int tictactoe(int socketClient) {
-
-    initSDL();
-    window = SDL_CreateWindow("MORPION",50,50,600,800,0);
-    renderer = SDL_CreateRenderer(window,-1,0);
+SDL_bool program_launched = SDL_TRUE;
+int clientSocket;
 
 
-    Button btn;
-    btn.beginX = 50;
-    btn.beginY = 50;
-    btn.endX = 250;
-    btn.endY = 150;
-    btn.actionType = 6;
-
-    int ** board;
-    int * row;
-    board=malloc(sizeof(int*)*3);
-
-    for (int i = 0; i < 3; ++i)
-    {
-        row=malloc(sizeof(int)*3);
-        board[i]=row;
-
-    }
-    for (int i = 0; i < 3; ++i)
-    {
-        for (int y = 0; y < 3; ++y)
-        {
-
-            board[i][y]=0;
-            SDL_Log("| %d ",board[i][y]);
-        }
-        SDL_Log("| \n");
-    }
-    int player=1;
-
-    char data[8];
-    int swap = 0;
-    while(program_launch){
+void * sdlListen(){
+    while(program_launched){
         SDL_Event event;
-        SDL_Log("BOUCLE DE JEU");
-        if (recv(socketClient, data, 8, 0) <= 0){
-            SDL_Log("Deconnecté du serveur ! \n");
-            break;
-        }
-        SDL_Log("RECU : %s\n", data);
-        player+=1;
-        printboard(board);
-        if (strcmp("YOURTURN", data) == 0) {
-            int px;
-            int py;
-            SDL_Log("C'est a vous de jouer !");
-            createTextZone(renderer,"C'est à vous de jouer !",0,20,150,40);
-            updateRenderer(renderer);
-            scanf("%d", &px);
-            send(socketClient, &px, sizeof(px), 0);
-            scanf("%d", &py);
-            send(socketClient, &py, sizeof(py), 0);
-            char data2[3];
-            if (recv(socketClient, data2, 3, 0) <= 0){
-                SDL_Log("Deconnecté du serveur ! \n");
-                break;
-            }
-            SDL_Log("RECU2 : %s\n", data2);
-            if(strstr(data2, "NOK")!= NULL){
-                SDL_Log("Coup Impossible ! \n");
-            }else{
-                SDL_Log("Coup Possible ! \n");
-                turn(board,player,px,py);
-            }
-            data[0] = '\0';
-        } else if (strcmp("WAITTURN", data) == 0) {
-            SDL_Log("C'est au tour de l'adversaire ! \n");
-            int px;
-            int py;
-            recv(socketClient, &px, sizeof(px), 0);
-            recv(socketClient, &py, sizeof(py), 0);
-            turn(board,player,px,py);
-            data[0] = '\0';
-        } else if (strcmp("YOUWIN!!", data) == 0) {
-            SDL_Log("Vous avez gagné ! \n");
-            program_launch = SDL_FALSE;
-        } else if (strcmp("YOULOSE!", data) == 0) {
-            SDL_Log("Vous avez perdu ! \n");
-            program_launch = SDL_FALSE;
-        }
-
+        //SDL_Log("BOUCLE DE JEU");
 
         while(SDL_PollEvent(&event)){
             switch(event.type){
                 case SDL_QUIT:
                     SDL_Log("SDL QUIT");
-                    program_launch = SDL_FALSE;
+                    program_launched = SDL_FALSE;
                     break;
                 case SDL_MOUSEBUTTONDOWN:
+                    SDL_Log("SDL BTN DOWN");
+                    NG_Event * buttonDown = malloc(sizeof(NG_Event));
+                    buttonDown->type = SDL;
+                    buttonDown->instructions = "BUTTONDOWN";
+                    sendEvent(buttonDown);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+void * networkListen(){
+    NG_Event * disconnectEvent = malloc(sizeof(NG_Event));
+    disconnectEvent->type = NETWORK;
+    disconnectEvent->instructions = "DISCONNECT";
+
+    char data[8];
+    int player=1;
+    while (program_launched) {
+        if (recv(clientSocket, data, 8, 0) <= 0){
+            sendEvent(disconnectEvent);
+            break;
+        }
+        NG_Event receivedDataEvent;
+        receivedDataEvent.type = NETWORK;
+        receivedDataEvent.instructions = data;
+    }
+
+
+
+}
+
+int tictactoe(int socketClient) {
+    clientSocket = socketClient;
+    initSDL();
+    window = SDL_CreateWindow("MORPION",50,50,600,600,0);
+    renderer = SDL_CreateRenderer(window,-1,0);
+
+    pthread_t network_listener;
+    pthread_create(&network_listener, NULL, networkListen, NULL);
+    pthread_t sdl_listener;
+    pthread_create(&sdl_listener,NULL,sdlListen,NULL);
+
+    changeColor(renderer,255,255,255);
+    createFilledRectangle(0,0,600,600,renderer);
+    changeColor(renderer,0,0,0);
+    createFilledRectangle(0,195,600,10,renderer);
+    createFilledRectangle(0,395,600,10,renderer);
+    createFilledRectangle(195,0,10,600,renderer);
+    createFilledRectangle(395,0,10,600,renderer);
+    updateRenderer(renderer);
+
+    // TODO create board in SDL, we don't need the board
+//    int ** board;
+//    int * row;
+//    board=malloc(sizeof(int*)*3);
+//
+//    for (int i = 0; i < 3; ++i)
+//    {
+//        row=malloc(sizeof(int)*3);
+//        board[i]=row;
+//
+//    }
+//    for (int i = 0; i < 3; ++i)
+//    {
+//        for (int y = 0; y < 3; ++y)
+//        {
+//            board[i][y]=0;
+//            SDL_Log("| %d ",board[i][y]);
+//        }
+//        SDL_Log("| \n");
+//    }
+
+
+    int swap = 0;
+
+    while (program_launched){
+        NG_Event event;
+        while (listenAllEvents(&event)){
+            if(event.type==SDL){
+                if(strcmp(event.instructions,"BUTTONDOWN")==0) {
                     SDL_Log("SDL button down %d",swap);
                     if(swap%2==0) {
                         changeColor(renderer,255,0,0);
@@ -130,28 +126,65 @@ int tictactoe(int socketClient) {
                         changeColor(renderer,0,0,255);
                     }
                     swap++;
-                    createFilledRectangle(btn.beginX,btn.beginY,btn.endX-btn.beginX,btn.endY-btn.beginY,renderer);
+                    createFilledRectangle(50,50,200,120,renderer);
                     updateRenderer(renderer);
-                    break;
-                default:
-                    break;
+                }
             }
+//            else if(event.type == NETWORK){
+//                if(strcmp(event.instructions,"DISCONNECTED") == 0){
+//                    SDL_Log("DECONNECTE DU SERVEUR");
+//                } else {
+//
+//                    //printf("RECU : %s\n", data);
+//
+//                    player+=1;
+//                    printboard(board);
+//                    if (strcmp("YOURTURN", event.instructions) == 0) {
+//                        int px;
+//                        int py;
+//                        printf("C'est a vous de jouer ! \n");
+//                        scanf("%d", &px);
+//                        send(socketClient, &px, sizeof(px), 0);
+//                        scanf("%d", &py);
+//                        send(socketClient, &py, sizeof(py), 0);
+//                        char data2[3];
+//                        if (recv(socketClient, data2, 3, 0) <= 0){
+//                            sendEvent(disconnectEvent);
+//                            break;
+//                        }
+//                        printf("RECU2 : %s\n", data2);
+//                        if(strstr(data2, "NOK")!= NULL){
+//                            printf("Coup Impossible ! \n");
+//                        }else{
+//                            printf("Coup Possible ! \n");
+//                            turn(board,player,px,py);
+//                        }
+//                        event.instructions[0] = '\0';
+//                    } else if (strcmp("WAITTURN", event.instructions) == 0) {
+//                        printf("C'est au tour de l'adversaire ! \n");
+//                        int px;
+//                        int py;
+//                        recv(socketClient, &px, sizeof(px), 0);
+//                        recv(socketClient, &py, sizeof(py), 0);
+//                        turn(board,player,px,py);
+//                        event.instructions[0] = '\0';
+//                    } else if (strcmp("YOUWIN!!", event.instructions) == 0) {
+//                        printf("Vous avez gagné ! \n");
+//                        program_launched = SDL_FALSE;
+//                    } else if (strcmp("YOULOSE!", event.instructions) == 0) {
+//                        printf("Vous avez perdu ! \n");
+//                        program_launched = SDL_FALSE;
+//                    }
+//                }
+//            }
         }
     }
     quitSDL(renderer,window);
-
-
-    free(row);
-    free(board);
-
-    //printf("STRING RECU : %s\n", buffer);
     close(socketClient);
-
     return 0;
 }
 
-void printboard(int ** board)
-{
+void printboard(int ** board) {
     for (int i = 2; i >=0; --i)
     {
         for (int y = 0; y < 3; ++y)
