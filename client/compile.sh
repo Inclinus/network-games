c6b2676aa444a76866b5ecd4203d9e0f15d74295
Noam DE MASURE <demasure.noam@hotmail.fr>
2023-02-06 23:42:40 +0100

    fix compile.sh

--- client/compile.sh
+++ client/compile.sh
@@ -1,1 +1,1 @@
-gcc ../events/EventManager.c client.c tictactoe/tictactoe.c ../sdl-utils/SDLUtils.c $(sdl2-config --cflags --libs) -pthread -lSDL2_ttf -lSDL2_image -o client.out
+gcc ../events/EventManager.c client.c tictactoe/tictactoe.c ../sdl-utils/SDLUtils.c $(sdl2-config --cflags --libs) -pthread -lSDL2_ttf -lSDL2_image -lm -o client.out
