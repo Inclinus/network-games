e290bef4a695a084d8faf1591c99e2e367585f7b
Kyato <thibaut.lulinski@hotmail.fr>
2023-02-21 13:06:58 +0100

    fix mysql install in docker

--- network_games_bdd.sql
+++ network_games_bdd.sql
@@ -18,6 +18,7 @@
 CREATE DATABASE network_games_bdd;
 USE network_games_bdd;
 
+
 --
 -- Table structure for table `users`
 --
@@ -27,7 +28,13 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `login` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL
+  `password` varchar(100) DEFAULT NULL,
+  `nbWinTictactoe` int(11) DEFAULT 0,
+  `nbLooseTictactoe` int(11) DEFAULT 0,
+  `nbDrawTictactoe` int(11) DEFAULT 0,
+  `nbWinConnect4` int(11) DEFAULT 0,
+  `nbLooseConnect4` int(11) DEFAULT 0,
+  `nbDrawConnect4` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -37,7 +44,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('tibo','$2a$12$xymTDT2mpCEAgg9TuKo8LeC7XF1lByhy0HIGGATaDHV.4NHtUGVTa'),('noam','$2a$12$o6FAcVymzxOmw5c9/q.3AuqOUMUgKmZie2i9ztCBz3FAu9DyaC.g.'),('alex','$2a$12$BSJOt0gKHgoPgmLhE4sh8.r7JjSmaloSrOsakY7p12hc.vlzPiigK');
+INSERT INTO `users` VALUES ('tibo','$2a$12$xymTDT2mpCEAgg9TuKo8LeC7XF1lByhy0HIGGATaDHV.4NHtUGVTa',1,2,3,4,5,6),('noam','$2a$12$o6FAcVymzxOmw5c9/q.3AuqOUMUgKmZie2i9ztCBz3FAu9DyaC.g.',0,0,0,0,0,0),('alex','$2a$12$BSJOt0gKHgoPgmLhE4sh8.r7JjSmaloSrOsakY7p12hc.vlzPiigK',0,0,0,0,0,0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-18 10:39:57
+-- Dump completed on 2023-02-20  0:38:20
--- server/server.c
+++ server/server.c
@@ -174,7 +174,7 @@ int main() {
     int socketServer = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addrServer;
     //addrServer.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addrServer.sin_addr.s_addr = inet_addr("92.222.131.57");
+    addrServer.sin_addr.s_addr = inet_addr("0.0.0.0");
     addrServer.sin_family = AF_INET;
     addrServer.sin_port = htons(4444);
 
